6cd49b23594f41a77a1bf5186ed65c71ae8f3bd3
132ikl <132@ikl.sh>
2019-10-06 23:04:07 -0400

    --amend

--- build_scripts/components/opencv.sh
+++ build_scripts/components/opencv.sh
@@ -12,18 +12,18 @@ if ! ${DOCKER} ps >/dev/null; then
         exit 1
 fi
 
-if [ ! -e "cache/opencv-python_${OPENCV_VERSION}_armhf.deb" ]; then
-    ${DOCKER} build -t opsi-opencv opencv-build
-    ${DOCKER} run --rm --privileged \
-        --volume "$(pwd)":/packages \
-        --name "opencv-build_work" \
-        opsi-opencv \
-        bash -e -o pipefail -c "cd /packages/opencv-build/; OPENCV_VERSION=${OPENCV_VERSION} ./build.sh"
-    mkdir -p cache/
-    rm -rf cache/*
-    cp "packages/opencv-python_${OPENCV_VERSION}_armhf.deb" "cache/opencv-python_${OPENCV_VERSION}_armhf.deb"
-    cp "packages/opencv-libs_${OPENCV_VERSION}_armhf.deb" "cache/opencv-libs_${OPENCV_VERSION}_armhf.deb"
-else
-    cp "cache/opencv-python_${OPENCV_VERSION}_armhf.deb" "packages/opencv-python_${OPENCV_VERSION}_armhf.deb"
-    cp "cache/opencv-libs_${OPENCV_VERSION}_armhf.deb" "packages/opencv-libs_${OPENCV_VERSION}_armhf.deb"
-fi
+# if [ ! -e "cache/opencv-python_${OPENCV_VERSION}_armhf.deb" ]; then
+#     ${DOCKER} build -t opsi-opencv opencv-build
+#     ${DOCKER} run --rm --privileged \
+#         --volume "$(pwd)":/packages \
+#         --name "opencv-build_work" \
+#         opsi-opencv \
+#         bash -e -o pipefail -c "cd /packages/opencv-build/; OPENCV_VERSION=${OPENCV_VERSION} ./build.sh"
+#     mkdir -p cache/
+#     rm -rf cache/*
+#     cp "packages/opencv-python_${OPENCV_VERSION}_armhf.deb" "cache/opencv-python_${OPENCV_VERSION}_armhf.deb"
+#     cp "packages/opencv-libs_${OPENCV_VERSION}_armhf.deb" "cache/opencv-libs_${OPENCV_VERSION}_armhf.deb"
+# else
+#     cp "cache/opencv-python_${OPENCV_VERSION}_armhf.deb" "packages/opencv-python_${OPENCV_VERSION}_armhf.deb"
+#     cp "cache/opencv-libs_${OPENCV_VERSION}_armhf.deb" "packages/opencv-libs_${OPENCV_VERSION}_armhf.deb"
+# fi
